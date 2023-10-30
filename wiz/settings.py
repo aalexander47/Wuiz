710d175034d03ddb583ac7c1ea77d654f42d2a1b
Ajay singh <aalexander4700@gmail.com>
2023-10-30 19:51:23 +0530

    dated

--- wiz/settings.py
+++ wiz/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'staticfiles')
+STATIC_DIR=os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 
 
