9aa0bd72c09a83ac55c92b3e38d98a5ba62dac2f
Ajay singh <aalexander4700@gmail.com>
2023-10-30 22:00:43 +0530

    change

--- wiz/settings.py
+++ wiz/settings.py
@@ -84,12 +84,17 @@ WSGI_APPLICATION = 'wiz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, conn_health_checks=True),
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
