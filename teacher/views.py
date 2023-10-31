6ef0accaa3c1c8cba21d1aa59348afb3f9289684
Ajay singh <aalexander4700@gmail.com>
2023-10-31 15:39:19 +0530

    done

--- teacher/views.py
+++ teacher/views.py
@@ -121,7 +121,3 @@ def remove_question_view(request,pk):
     question=QMODEL.Question.objects.get(id=pk)
     question.delete()
     return HttpResponseRedirect('/teacher/teacher-view-question')
-
-# @login_required(login_url='teacherlogin')
-# @user_passes_test(is_teacher)
-# def view_student_marks
